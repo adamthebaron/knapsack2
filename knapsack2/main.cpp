123b0bd1e04c7d1320576b3cfd1c278a634409b0
adam <apk@pluvi.us>
2018-09-22 20:49:53 -0400

    Initial Commit

---
+++ knapsack2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  knapsack2
+//
+//  Created by adam on 9/22/18.
+//  Copyright © 2018 pluvius software. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
